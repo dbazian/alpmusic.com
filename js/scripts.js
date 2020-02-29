1166e862d54ce61816260dd1db27ff9374c44fe5
Darrell Bazian <59262029+dbazian@users.noreply.github.com>
2020-02-29 15:15:35 -0500

    javascript

--- js/scripts.js
+++ js/scripts.js
@@ -373,15 +373,12 @@ $(".mobile-team").click(function() {
   }
 });
 
+});
 
 // Mobile view hight
+
 window.addEventListener('resize', () => {
 
 let vh = window.innerHeight * 0.01;
-
+  
 document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-
-//END
-
-});
