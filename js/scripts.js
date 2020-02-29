8db2863839299899492487ed13e44f0fd4641c99
Darrell Bazian <59262029+dbazian@users.noreply.github.com>
2020-02-29 16:13:27 -0500

    nav

--- js/scripts.js
+++ js/scripts.js
@@ -377,3 +377,11 @@ $(".mobile-team").click(function() {
 
 });
 
+// Mobile view hight
+
+window.addEventListener('resize', () => {
+
+let vh = window.innerHeight * 0.01;
+  
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
