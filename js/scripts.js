81eefb337533a76dcd15915a4cb47c462f12bab7
Darrell Bazian <59262029+dbazian@users.noreply.github.com>
2020-02-29 21:56:24 -0500

    scroll snap removed

--- js/scripts.js
+++ js/scripts.js
@@ -378,10 +378,8 @@ $(".mobile-team").click(function() {
 // Mobile view hight
 
 window.addEventListener('resize', () => {
-
-  let vh = window.innerHeight * 0.01;
-    
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
   });
 
 });
